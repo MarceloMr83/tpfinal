57c5725da6c4e7035f1bf2c50e84ef52c5eca982
MarceloMr83 <51954324+MarceloMr83@users.noreply.github.com>
2020-12-04 17:38:39 -0300

    Archivo sql para crear la base de datos

---
+++ hotel.sql
@@ -0,0 +1,126 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         8.0.22 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para poo
+CREATE DATABASE IF NOT EXISTS `poo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `poo`;
+
+-- Volcando estructura para tabla poo.booking
+CREATE TABLE IF NOT EXISTS `booking` (
+  `idBooking` int NOT NULL AUTO_INCREMENT,
+  `checkIn` date NOT NULL,
+  `checkOut` date NOT NULL,
+  `createdAt` date NOT NULL,
+  `breakfastIncluded` tinyint(1) NOT NULL DEFAULT '0',
+  `parking` tinyint(1) NOT NULL DEFAULT '0',
+  `cost` float(12,2) NOT NULL,
+  `guest` int NOT NULL,
+  `room` int NOT NULL,
+  `version` int DEFAULT NULL,
+  `freeCancelation` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idBooking`),
+  KEY `guest_idx` (`guest`),
+  KEY `room_idx` (`room`),
+  CONSTRAINT `guest` FOREIGN KEY (`guest`) REFERENCES `user` (`idUser`),
+  CONSTRAINT `room` FOREIGN KEY (`room`) REFERENCES `room` (`idRoom`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla poo.booking: ~0 rows (aproximadamente)
+DELETE FROM `booking`;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+INSERT INTO `booking` (`idBooking`, `checkIn`, `checkOut`, `createdAt`, `breakfastIncluded`, `parking`, `cost`, `guest`, `room`, `version`, `freeCancelation`) VALUES
+	(1, '2020-12-01', '2020-12-15', '2020-12-04', 0, 0, 0.00, 18, 4, 0, 0);
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+
+-- Volcando estructura para tabla poo.cancellation
+CREATE TABLE IF NOT EXISTS `cancellation` (
+  `idCancellation` int NOT NULL AUTO_INCREMENT,
+  `createdAt` date NOT NULL,
+  `booking` int NOT NULL,
+  `version` int DEFAULT NULL,
+  PRIMARY KEY (`idCancellation`),
+  KEY `booking_idx` (`booking`),
+  CONSTRAINT `booking2` FOREIGN KEY (`booking`) REFERENCES `booking` (`idBooking`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla poo.cancellation: ~0 rows (aproximadamente)
+DELETE FROM `cancellation`;
+/*!40000 ALTER TABLE `cancellation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cancellation` ENABLE KEYS */;
+
+-- Volcando estructura para tabla poo.payment
+CREATE TABLE IF NOT EXISTS `payment` (
+  `idpayment` int NOT NULL AUTO_INCREMENT,
+  `createdAt` date NOT NULL,
+  `booking` int NOT NULL,
+  `card` varchar(45) NOT NULL,
+  `cardNumber` varchar(45) NOT NULL,
+  `version` int DEFAULT NULL,
+  PRIMARY KEY (`idpayment`),
+  KEY `booking_idx` (`booking`),
+  CONSTRAINT `booking` FOREIGN KEY (`booking`) REFERENCES `booking` (`idBooking`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla poo.payment: ~0 rows (aproximadamente)
+DELETE FROM `payment`;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+
+-- Volcando estructura para tabla poo.room
+CREATE TABLE IF NOT EXISTS `room` (
+  `idRoom` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` float NOT NULL,
+  `occupancy` int NOT NULL,
+  `availability` int NOT NULL,
+  `facilities` varchar(45) NOT NULL,
+  `version` int DEFAULT NULL,
+  PRIMARY KEY (`idRoom`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla poo.room: ~5 rows (aproximadamente)
+DELETE FROM `room`;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` (`idRoom`, `name`, `price`, `occupancy`, `availability`, `facilities`, `version`) VALUES
+	(1, 'SIMPLE', 1000, 1, 3, 'TV', 0),
+	(2, 'SIMPLE', 2000, 1, 3, 'TV COCINA', 0),
+	(3, 'DOBLE', 4000, 2, 3, 'TV', 0),
+	(4, 'DOBLE', 5000, 2, 3, 'TV COCINA AIRE', 0),
+	(5, 'TRIPLE', 6000, 3, 3, 'TV', 0),
+	(6, 'TRIPLE', 7000, 3, 3, 'TV COCINA AIRE', 0);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+
+-- Volcando estructura para tabla poo.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `birthDate` date NOT NULL,
+  `nationality` varchar(45) NOT NULL,
+  `version` int DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla poo.user: ~0 rows (aproximadamente)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`idUser`, `email`, `password`, `firstName`, `lastName`, `birthDate`, `nationality`, `version`) VALUES
+	(18, 'marcelomrios83@gmail.com', '$2a$10$JIX.Ph8ByLCKxx.g8tmTOugN8PYSqepd6kLFwboEQXl3N9qCHYRfu', 'Marcelo', 'Rios', '2020-11-01', 'ARG', 0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
